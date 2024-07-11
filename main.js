870e0cc600155882137cc03dd6141c6c0b049712
israthnr <israthnr.20@uom.lk>
2024-07-11 13:33:52 +0530

    routes structure

--- main.js
+++ main.js
@@ -9,6 +9,27 @@ app.get("/", (req,res) => {
     });
 });
 
+//CRUD funcionality of movies
+// R - for reading
+app.get('/movies',() => {
+
+});
+
+// C - for creating movies
+app.post('/movies',() => {
+
+});
+
+// U - update movies
+app.put('/movies/:id',() => {
+
+});
+
+// D- for deleting movies
+app.delete('/movies/:id',() => {
+
+});
+
 app.listen(PORT, () => {
     console.log(`The server is running at http://localhost:${PORT}`);
 });
