3048a0d2697ee0d6becb1c080a1980db484bd556
israthnr <israthnr.20@uom.lk>
2024-07-11 12:22:37 +0530

    create the app structure

---
+++ README.md
@@ -0,0 +1,12 @@
+# api_crud
+
+provide a crud api for react.js
+
+### important commands
+
+1. `npm install`
+2. `npm run server`
+3. `git init`
+4. `git add .`
+5. `git commit -m "commit message"`
+6. `git push -u origin main` or `git push`
---
+++ main.js
@@ -0,0 +1,14 @@
+import express from "express";
+
+const app = express();
+const PORT = 6969;
+
+app.get("/", (req,res) => {
+    res.json({
+        msg: "Hello Rahna"
+    });
+});
+
+app.listen(PORT, () => {
+    console.log(`The server is running at http://localhost:${PORT}`);
+});
